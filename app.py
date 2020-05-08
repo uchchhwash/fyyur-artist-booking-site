9a71b50d59ee57669d6d8ca285e908b2f1dd3528
uchchhwash <uchchhwash.git@gmail.com>
2020-05-08 13:35:58 +0600

    search for venue implemented

--- app.py
+++ app.py
@@ -82,13 +82,12 @@ def search_venues():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for Hop should return "The Musical Hop".
   # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
+  search_term = request.form.get('search_term', '')
+  result = Venue.query.filter(Venue.name.ilike(f'%{search_term}%'))
+
   response={
-    "count": 1,
-    "data": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
+    "count": result.count(),
+    "data": result
   }
   return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
 

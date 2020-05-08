84fb93c161cca3723d5c7cf364fafe3d733cea2f
uchchhwash <uchchhwash.git@gmail.com>
2020-05-08 13:31:16 +0600

    search for artist implemented

--- app.py
+++ app.py
@@ -229,13 +229,12 @@ def search_artists():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
+  search_term = request.form.get('search_term', '')
+  # filter artists by case insensitive search
+  result = Artist.query.filter(Artist.name.ilike(f'%{search_term}%'))
   response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
+    "count": result.count(),
+    "data": result
   }
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 

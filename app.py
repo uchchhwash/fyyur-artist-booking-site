4fbce938da8a9ca6560c567288ca712ea7ed01b1
uchchhwash <uchchhwash.git@gmail.com>
2020-05-08 15:22:34 +0600

    code refactoring

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
-# import logging
-# from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
 from models import *
